a9f952dbf087e51e2bf4e2dfb6f5c7b8ce103abe
Daniel Atonal <danielatonal8@gmail.com>
2023-08-04 13:47:01 -0600

    Update useClima.js

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -21,7 +21,7 @@ export default function useClima(){
             mostrarSpinner.value=true
             clima.value={}
 
-            const url= `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const url= `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
 
             // const response= await fetch(url)
             // const data= await response.json()
